dd9eb41955ffe4ce22046631fcadf4fbad3460c6
Ubuntu <ubuntu@ubuntu-18.04.05--vm>
2021-03-22 12:53:03 +0100

    Add gopigo_image_pub_sub.py

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/ubuntu/robotica2021/rubot_opencv_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/ubuntu/robotica2021/gopigo_pc_ws/devel;/home/ubuntu/robotica2021/rubot_tutorial_ws/devel;/opt/ros/melodic'.split(';') if '/home/ubuntu/robotica2021/gopigo_pc_ws/devel;/home/ubuntu/robotica2021/rubot_tutorial_ws/devel;/opt/ros/melodic' != '' else []
--- build/vision/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/robotica2021/rubot_opencv_ws/src/vision
-# Build directory: /home/ubuntu/robotica2021/rubot_opencv_ws/build/vision
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
